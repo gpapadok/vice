316d61daf976a4ef3c22bfbdb5567d164660575e
gpapadok <giorgos.papadokostakis@proton.me>
2026-08-10 19:36:09 +0300

    fix: rebind vice-map when vice-key-prefix is customized
    Previously vice-map's keybinding was set once at load time via a
    plain defvar initializer, so changing vice-key-prefix afterward had
    no effect on the running keymap. Add a :set function that removes
    the old binding and installs the new one.

--- vice-mode.el
+++ vice-mode.el
@@ -56,9 +56,17 @@
   :prefix "vice-")
 
 (defcustom vice-key-prefix "C-c v"
-  "Key prefix bound to `vice-dispatch' in `vice-map'."
+  "Key prefix bound to `vice-dispatch' in `vice-map'.
+Setting this via `customize-set-variable' or `setopt' rebinds
+`vice-map' to the new key, removing the old binding."
   :type 'string
-  :group 'vice)
+  :group 'vice
+  :set (lambda (sym val)
+         (let ((old (and (boundp sym) (symbol-value sym))))
+           (set-default sym val)
+           (when (boundp 'vice-map)
+             (when old (define-key vice-map (kbd old) nil))
+             (define-key vice-map (kbd val) #'vice-dispatch)))))
 
 ;; Text objects
 
